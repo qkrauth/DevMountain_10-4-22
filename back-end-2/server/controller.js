5dfce641c514734015b7c45f0df940ef0490db99
Quinten Krauth <quintenkrauth19@gmail.com>
2022-10-04 17:02:12 -0400

    lab attempt

---
+++ back-end-2/server/controller.js
@@ -0,0 +1,15 @@
+module.exports = {
+    getHouses: (req, res) => {
+
+    },
+    createHouse: (req, res) => {
+
+    },
+    updateHouse: (req, res) => {
+
+    },
+    deleteHouse: (req, res) => {
+        
+    }
+
+}
